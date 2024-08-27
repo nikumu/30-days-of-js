48bdfb4e45532d34d3d9952a051fb34e5279bd4c
André Bianchi <andrecarlosbb@gmail.com>
2024-08-27 16:13:40 -0300

    Create porcentagem.js

---
+++ dia-02/porcentagem.js
@@ -0,0 +1,6 @@
+const porcentagem = (numerador, denominador) => {
+    var resultado = numerador / denominador
+    console.log(numerador + " equivale a " + resultado * 100 + "% de " + denominador)
+}
+
+porcentagem(50, 100)
